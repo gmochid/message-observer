e901ed67d3fe44a5369dca1fd033d76592e2546c
Muhammad S. Maromi <msmaromi@gmail.com>
2014-11-30 09:16:35 +0700

    ganti format

--- app/views/emails/notif.blade.php
+++ app/views/emails/notif.blade.php
@@ -1,3 +1,7 @@
-<!--<h1>Hi, !</h1>-->
- 
-<p>Surat anda telah sampai pada {{$status->detail}}</p>
+<p>Dear pengirim,</p>
+<p>Surat anda telah sampai di {{$status->detail}} pada {{$status->updated_at}}</p>
+<p>
+Regards,<br>
+Mailon Admin
+</p>
+
