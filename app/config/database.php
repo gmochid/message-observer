f897b370bcfb366abfccaeb70119b5905b9f5def
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2015-02-16 16:03:22 +0700

    prepare to deploy on cpanel

--- app/config/database.php
+++ app/config/database.php
@@ -1,12 +1,5 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return array(
 
 	/*
@@ -33,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'pgsql',
+	'default' => 'mysql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -70,16 +63,16 @@ return array(
 			'prefix'    => '',
 		),
 
-		'pgsql' => array(
-	        'driver'   => 'pgsql',
-	        'host'     => $host,
-	        'database' => $database,
-	        'username' => $username,
-	        'password' => $password,
-	        'charset'  => 'utf8',
-	        'prefix'   => '',
-	        'schema'   => 'public',
-	    ),
+		// 'pgsql' => array(
+	 //        'driver'   => 'pgsql',
+	 //        'host'     => $host,
+	 //        'database' => $database,
+	 //        'username' => $username,
+	 //        'password' => $password,
+	 //        'charset'  => 'utf8',
+	 //        'prefix'   => '',
+	 //        'schema'   => 'public',
+	 //    ),
 
 	),
 
