6d817402d31213bb28aafdcc1613722719d8c684
Abdurrosyid Broto Handoyo <arrosyidbh@gmail.com>
2015-02-20 19:52:39 +0700

    remove pg-sql

--- app/config/database.php
+++ app/config/database.php
@@ -63,17 +63,6 @@ return array(
 			'prefix'    => '',
 		),
 
-		// 'pgsql' => array(
-	 //        'driver'   => 'pgsql',
-	 //        'host'     => $host,
-	 //        'database' => $database,
-	 //        'username' => $username,
-	 //        'password' => $password,
-	 //        'charset'  => 'utf8',
-	 //        'prefix'   => '',
-	 //        'schema'   => 'public',
-	 //    ),
-
 	),
 
 	/*
